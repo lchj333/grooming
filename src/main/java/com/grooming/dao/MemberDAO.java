e7351623bb810c987108b7a51f5f4f782738eca1
Re2485 <buddha2485@gmail.com>
2020-06-03 14:47:54 +0900

    MemberDAO 수정
    MemberDAO 수정

--- src/main/java/com/grooming/dao/MemberDAO.java
+++ src/main/java/com/grooming/dao/MemberDAO.java
@@ -1,8 +1,40 @@
 package com.grooming.dao;
 
-import com.grooming.dto.MemberVO;
+import java.util.List;
 
-public interface MemberDAO {
-	public String getTime();
-	public void insertMember(MemberVO mvo);
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.grooming.dto.MemberDTO;
+
+
+@Repository
+public class MemberDAO {
+	
+	@Inject
+	SqlSession ss;
+
+	public void setSs(SqlSession ss) {
+		this.ss = ss;
+	}
+	
+	// 회원가입 메소드
+	public void joinMember(MemberDTO memberDto) {
+		ss.insert("joinMember", memberDto);
+	}
+	
+	// 회원전체 전체 조회 메소드
+	public List<MemberDTO> selectMemberAll(){
+		return ss.selectList("selectMemberAll");
+	}
+	
+	// ID로 회원 개별 조회 메소드
+	public MemberDTO selectOne(String id) {
+		return ss.selectOne("selectMemberOne", id);
+	}
+	
+	
+	
 }
