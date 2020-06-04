84aa8e251c06565acdc2364a0b5c300743d7f06a
Re2485 <buddha2485@gmail.com>
2020-06-04 11:38:26 +0900

    DesignerController 생성
    DesignerController 생성

--- src/main/java/com/grooming/control/DesignerController.java
+++ src/main/java/com/grooming/control/DesignerController.java
@@ -3,7 +3,10 @@ package com.grooming.control;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.grooming.dao.DesignerDAO;
 import com.grooming.dto.DesignerDTO;
@@ -14,13 +17,13 @@ public class DesignerController {
 	@Inject
 	DesignerDAO dao;
 	
-	@RequestMapping(value = "/designer")
-	public String designerJoin(DesignerDTO designerDto) {
+	@RequestMapping(value = "/designerJoin")
+	public String designerJoin(@ModelAttribute("mb_id") DesignerDTO designerDto) {
 		
 		dao.joinDesigner(designerDto);
 		
-		return "home";
+		
+		return "redirect:/selectMemberAll";
 	}
-	
-	
+
 }
