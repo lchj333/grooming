e838f5dd316d03ef8fec69464044851f6bfe215e
Re2485 <buddha2485@gmail.com>
2020-06-04 17:34:15 +0900

    LoginDAO 생성
    LoginDAO 생성

---
+++ src/main/java/com/grooming/dao/LoginDAO.java
@@ -0,0 +1,31 @@
+package com.grooming.dao;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.grooming.dto.LoginDTO;
+
+@Repository
+public class LoginDAO {
+	
+	@Inject
+	SqlSession ss;
+
+	public void setSs(SqlSession ss) {
+		this.ss = ss;
+	}
+	
+	// ID,PW 존재여부 처리 메소드
+	public int loginCheck(LoginDTO loginDto) {
+		return ss.selectOne("loginCheck", loginDto);
+	}
+	
+	
+	// 로그인 처리 메소드
+	public LoginDTO loginOk(LoginDTO loginDto) {
+		return ss.selectOne("LoginOk", loginDto);
+	}
+	
+}
