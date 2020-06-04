98fef60ac21aa36fc1665a842b1434e0120985f1
Re2485 <buddha2485@gmail.com>
2020-06-04 11:33:33 +0900

    testDesigner 수정
    testDesigner 내용 수정

--- src/test/java/com/grooming/control/DesignerTest.java
+++ src/test/java/com/grooming/control/DesignerTest.java
@@ -1,5 +1,33 @@
 package com.grooming.control;
 
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.grooming.dao.DesignerDAO;
+import com.grooming.dto.DesignerDTO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/root-context.xml")
 public class DesignerTest {
 
+	@Inject DesignerDAO designerDao;
+	
+	@Test
+	public void testDesigner() {
+		DesignerDTO dto = new DesignerDTO();
+		
+		dto.setMb_id("aaaa");
+		dto.setDe_licence("나는 짱짱맨 미용사");
+		dto.setDe_approval("N");
+		dto.setDe_point(1000);
+		
+		designerDao.joinDesigner(dto);
+		
+		System.out.println("미용사 등록 성공");
+	}
+	
 }
