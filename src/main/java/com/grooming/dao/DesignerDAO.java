13c3a884fc5292695cd2ec197a4e057d7bb28242
Re2485 <buddha2485@gmail.com>
2020-06-08 12:49:16 +0900

    DesignerDAO 수정

--- src/main/java/com/grooming/dao/DesignerDAO.java
+++ src/main/java/com/grooming/dao/DesignerDAO.java
@@ -1,5 +1,7 @@
 package com.grooming.dao;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -21,5 +23,21 @@ public class DesignerDAO {
 	public void joinDesigner(DesignerDTO designerDto) {
 		ss.insert("joinDesigner", designerDto);
 	}
+	
+	// 미용사 멤버 전체 조회
+	public List<DesignerDTO> selectDesignerAll(){
+		return ss.selectList("selectDesignerAll");
+	}
+	
+	// 미용사 신청한 사람 목록 받아오기
+	public List<DesignerDTO> designerApplication(){
+		return ss.selectList("designerApplication");
+	}
+	
+	// 디자이너 de_approval 승인하는 메소드
+	public void designerAgree(DesignerDTO designerDto) {
+		ss.update("designerAgree", designerDto);
+	}
+	
 	
 }
