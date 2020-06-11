d68264046c70a553ce4db3c17c2e5aafa42c5061
Re2485 <buddha2485@gmail.com>
2020-06-11 19:14:28 +0900

    LoginController 수정

--- src/main/java/com/grooming/control/LoginController.java
+++ src/main/java/com/grooming/control/LoginController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.grooming.dao.AdminDAO;
 import com.grooming.dao.DesignerDAO;
 import com.grooming.dao.LoginDAO;
+import com.grooming.dto.AdminDTO;
 import com.grooming.dto.DesignerDTO;
 import com.grooming.dto.LoginDTO;
 import com.grooming.dto.MemberDTO;
@@ -25,24 +27,40 @@ public class LoginController {
 	LoginDAO dao;
 	@Inject
 	DesignerDAO dd;
+	@Inject
+	AdminDAO ad;
 	
-	@RequestMapping(value = "login", method = RequestMethod.GET)
-	public String loginGet(@ModelAttribute("loginDto") MemberDTO memberDto) {
+	@RequestMapping(value={ "/login", "/adlogin" }, method = RequestMethod.GET)
+	public String loginGet(@ModelAttribute("loginDto") MemberDTO memberDto,
+						   @ModelAttribute("adminDto") AdminDTO adminDto) {
 		return "loginForm";
 	}
 	
 	
-	@RequestMapping(value="login", method = RequestMethod.POST)
+	@RequestMapping(value={ "/login", "/adlogin" }, method = RequestMethod.POST)
 	public ModelAndView loginCheck(@ModelAttribute MemberDTO memberDto, 
 								   @ModelAttribute DesignerDTO designerDto,
+								   @ModelAttribute AdminDTO adminDto,
 								   HttpSession session, HttpServletRequest req) {
+		
 		SecurityService securityService = new SecurityService();
+		
 		String mb_id = req.getParameter("mb_id");
 		String pw = req.getParameter("mb_pw");
+		
+		adminDto.setAd_id(mb_id);
+		adminDto.setAd_pw(pw);
+		
 		String mb_pw = securityService.encryptSHA256(pw);
+		
+		
 		memberDto.setMb_pw(mb_pw);
 		designerDto.setMb_id(mb_id);
+		
+		
 		int check = dao.loginCheck(memberDto);
+		int adcheck = ad.adminCheck(adminDto);
+		
 		ModelAndView mav = new ModelAndView();
 		
 		
@@ -51,13 +69,22 @@ public class LoginController {
 			session.setAttribute("login", dao.loginOk(memberDto));
 			session.setAttribute("dInfo", dd.designerInfo(designerDto));
 			
-			System.out.println(dao.loginOk(memberDto));
-			System.out.println(memberDto.getMb_pw());
-			System.out.println(memberDto.getMb_id());
-			mav.setViewName("loginOk");
-		}else if (check == 0) {
+			
+			mav.setViewName("/mypage/mypage_nav");
+			mav.setViewName("/mypage/grooming_user_profile");
+		}else if (check == 0 || adcheck == 0) {
+			
+			
 			session.setAttribute("login", null);
-			mav.setViewName("loginOk");
+			mav.setViewName("redirect:/login");
+			
+		}
+		
+		if(adcheck == 1 ) {
+			
+			session.setAttribute("admin", ad.adminloginOk(adminDto));
+			
+			mav.setViewName("home");
 			
 		}
 		
@@ -72,7 +99,7 @@ public class LoginController {
 		
 		session.invalidate();
 		
-		return "login";
+		return "loginForm";
 	}
 	
 }
