d2d0b9ad73a2ada3fabe3fef77ed43021d89cc1e
Re2485 <buddha2485@gmail.com>
2020-06-11 11:04:34 +0900

    LoginController 수정

--- src/main/java/com/grooming/control/LoginController.java
+++ src/main/java/com/grooming/control/LoginController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.grooming.dao.DesignerDAO;
 import com.grooming.dao.LoginDAO;
+import com.grooming.dto.DesignerDTO;
 import com.grooming.dto.LoginDTO;
 import com.grooming.dto.MemberDTO;
 import com.grooming.service.SecurityService;
@@ -21,31 +23,37 @@ public class LoginController {
 
 	@Inject
 	LoginDAO dao;
+	@Inject
+	DesignerDAO dd;
 	
 	@RequestMapping(value = "login", method = RequestMethod.GET)
-	public String loginGet(@ModelAttribute("loginDto") LoginDTO loginDto) {
+	public String loginGet(@ModelAttribute("loginDto") MemberDTO memberDto) {
 		return "loginForm";
 	}
 	
 	
 	@RequestMapping(value="login", method = RequestMethod.POST)
-	public ModelAndView loginCheck(@ModelAttribute LoginDTO loginDto,HttpSession session, HttpServletRequest req) {
+	public ModelAndView loginCheck(@ModelAttribute MemberDTO memberDto, 
+								   @ModelAttribute DesignerDTO designerDto,
+								   HttpSession session, HttpServletRequest req) {
 		SecurityService securityService = new SecurityService();
+		String mb_id = req.getParameter("mb_id");
 		String pw = req.getParameter("mb_pw");
 		String mb_pw = securityService.encryptSHA256(pw);
-		
-		loginDto.setMb_pw(mb_pw);
-		int check = dao.loginCheck(loginDto);
+		memberDto.setMb_pw(mb_pw);
+		designerDto.setMb_id(mb_id);
+		int check = dao.loginCheck(memberDto);
 		ModelAndView mav = new ModelAndView();
 		
 		
 		if(check ==1) {
 			
-			session.setAttribute("login", dao.loginOk(loginDto));
+			session.setAttribute("login", dao.loginOk(memberDto));
+			session.setAttribute("dInfo", dd.designerInfo(designerDto));
 			
-			System.out.println(dao.loginOk(loginDto));
-			System.out.println(loginDto.getMb_pw());
-			System.out.println(loginDto.getMb_id());
+			System.out.println(dao.loginOk(memberDto));
+			System.out.println(memberDto.getMb_pw());
+			System.out.println(memberDto.getMb_id());
 			mav.setViewName("loginOk");
 		}else if (check == 0) {
 			session.setAttribute("login", null);
